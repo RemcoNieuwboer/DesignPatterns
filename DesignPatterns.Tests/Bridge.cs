5c03f83398632fd47e997e472c66767eff357e5c
RemcoNieuwboer <remco.nieuwboer@ziggo.nl>
2016-04-10 13:19:29 +0200

    adding Bridge Design Pattern

--- DesignPatterns.Tests/Bridge.cs
+++ DesignPatterns.Tests/Bridge.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using DesignPatterns;
+using DesignPatterns.Classes.Bridge;
 
 namespace DesignPatterns.Tests
 {
@@ -8,8 +8,12 @@ namespace DesignPatterns.Tests
     public class BridgeTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Bridge()
         {
+            IBridge objectversion1 = new ObjectVersion1();
+            var objectversion2 = new ObjectVersion2();
+            Assert.AreEqual(new AbstractionObject(objectversion1).Operation(), "Operations that has to be run always + Operaration specific for version 1 of the object");
+            Assert.AreEqual(new AbstractionObject(objectversion2).Operation(), "Operations that has to be run always + Operaration specific for version 2 of the object");
         }
     }
 }
