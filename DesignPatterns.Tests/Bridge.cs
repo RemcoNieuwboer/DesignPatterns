7811f83298a803d7e3680ac2df4d1ee2afe97747
RemcoNieuwboer <remco.nieuwboer@ziggo.nl>
2016-04-10 12:25:27 +0200

    Initial

---
+++ DesignPatterns.Tests/Bridge.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DesignPatterns;
+
+namespace DesignPatterns.Tests
+{
+    [TestClass]
+    public class BridgeTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
