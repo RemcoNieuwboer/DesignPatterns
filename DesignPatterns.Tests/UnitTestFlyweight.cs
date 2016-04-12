49aa87f5fbecc8671bd56d862055eca61017894f
RemcoNieuwboer <remco.nieuwboer@ziggo.nl>
2016-04-12 22:28:13 +0200

    Added UnitTestFlyweight

---
+++ DesignPatterns.Tests/UnitTestFlyweight.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DesignPatterns.Classes.Flyweight;
+
+namespace DesignPatterns.Tests
+{
+    [TestClass]
+    public class UnitTestFlyweight
+    {
+        [TestMethod]
+        public void Flyweight()
+        {
+            FLyweightFactory factory = new FLyweightFactory();
+            factory["item1"].IntrinsicState = "somevalue";
+            Assert.AreEqual(factory.Count(), 1);
+
+            factory["item2"].IntrinsicState = "another value";
+            Assert.AreEqual(factory.Count(), 2);
+
+            Assert.AreEqual(factory["item1"].IntrinsicState, "somevalue");
+            Assert.AreEqual(factory["item2"].IntrinsicState, "another value");
+            Assert.AreEqual(factory.Count(), 2);
+
+            factory["item1"].Operation("somevalue changed");
+            factory["item2"].Operation("another value changed");
+            Assert.AreEqual(factory.Count(), 2);
+
+            Assert.AreEqual(factory["item1"].IntrinsicState, "somevalue changed");
+            Assert.AreEqual(factory["item2"].IntrinsicState, "another value changed");
+            Assert.AreEqual(factory.Count(), 2);
+        }
+    }
+}
